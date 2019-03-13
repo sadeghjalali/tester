ff9b36dc1ad5a930a7a0c219abe9ae73acd2321f
sadeghjalali <s.jalali.tsu@gmail.com>
2019-03-13 15:47:33 +0330

    Updates

---
+++ precache-manifest.2c57d3e0b13594f9c7e268360fb2ed7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.699f89d9.js"
+  },
+  {
+    "revision": "1804cf8e1b4e2b78204c",
+    "url": "/resume/static/js/main.1804cf8e.chunk.js"
+  },
+  {
+    "revision": "5a89b27bbba678df164f",
+    "url": "/resume/static/js/2.5a89b27b.chunk.js"
+  },
+  {
+    "revision": "1804cf8e1b4e2b78204c",
+    "url": "/resume/static/css/main.6bc8d8b0.chunk.css"
+  },
+  {
+    "revision": "c089542bf4c02042aa5227c8e9118258",
+    "url": "/resume/index.html"
+  }
+];
